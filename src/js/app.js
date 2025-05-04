90400fb9f6445d325f283402bc27a7c69955f967
Pruthviraj <visitmahuva@gmail.com>
2025-05-04 11:43:42 +0530

    make javascript better

--- src/js/app.js
+++ src/js/app.js
@@ -1,28 +1,55 @@
 class App {
-
-    isNavOpen = false;
-    navElement = null;
+    #isNavOpen = false;
+    #navElement = null;
+    #language = "english";
+    #url = "https://visitmahuva.github.io";
 
     constructor() {
-        this.isNavOpen = false; // nav is not open
-        this.navElement = document.querySelector(".nav")
-        this.language = "english";
-        this.url = "https://visitmahuva.github.io";
+        this.#navElement = document.querySelector(".nav");
+
+        if (!this.#navElement) {
+            console.error("Nav element not found!");
+        }
+
+        this.init();
+    }
+
+    init() {
+        this.#setCopyright();
+        this.#attachNavToggleHandler();
     }
 
     toggleNav() {
+        if (!this.#navElement) return;
 
-        if (!this.isNavOpen) {
-            this.navElement.style.width = "100%";
-            this.isNavOpen = true;
+        this.#isNavOpen = !this.#isNavOpen;
+        this.#navElement.style.width = this.#isNavOpen ? "100%" : "0%";
+    }
+
+    #setCopyright() {
+        const footer = document.querySelector(".footer");
+
+        if (!footer) {
+            console.error("Footer element not found!");
+            return;
         }
-        else {
-            this.navElement.style.width = "0%";
-            this.isNavOpen = false;
 
+        const currentYear = new Date().getFullYear();
+        footer.innerHTML = `<p title="Copyright by Visit Mahuva">&copy; Visit Mahuva - ${currentYear}</p>`;
+    }
+
+    #attachNavToggleHandler() {
+        const menuButton = document.querySelector(".menu");
+
+        if (!menuButton) {
+            console.error("Menu button not found!");
+            return;
         }
 
+        menuButton.addEventListener("click", () => this.toggleNav());
     }
 }
 
-const app = new App();
+document.addEventListener("DOMContentLoaded", () => {
+    const app = new App();
+});
